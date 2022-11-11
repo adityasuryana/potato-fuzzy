0f02b8fe52d11e8b6fa639186fbdb9fdf7744273
adityasuryana <arbeitaditya@gmail.com>
2022-11-11 20:07:24 +0700

    add laporan

---
+++ laporan-permintaan.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+if($_SESSION['status']!="loggedin"){
+  header("location:index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laporan Data Permintaan</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php
+  		require_once('connection.php');
+  		$sql = "SELECT * FROM permintaan";
+  		$result = mysqli_query($conn, $sql);
+  	?>
+    <nav class="navbar navbar-expand-lg navbar-light">
+       <div class="container">
+         <p class="navbar-brand mx-auto mb-0">kentang.</p>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+           <div class="navbar-nav ms-auto">
+             <a class="nav-link text-danger mx-2" href="logout.php">Logout</a>
+           </div>
+         </div>
+       </div>
+     </nav>
+
+     <div class="container">
+       <div class="row">
+         <div class="col-xxl-2 col-xl-2 col-lg-2 d-sm-none d-md-none d-lg-block"></div>
+
+         <div class="col-xxl-8 col-xl-8 col-lg-8 col-md-12 col-12">
+           <div class="d-flex">
+             <a href="owner.php" class="my-auto text-dark"><i class="fa-solid fa-arrow-left fs-20 me-3"></i></a>
+             <h2 class="header-title mt-4 mb-4">Laporan Data Permintaan</h2>
+           </div>
+           
+           <div class="table-data">
+     				<div class="order">
+     					<table id="table">
+     						<thead>
+     							<tr>
+     								<th>ID</th>
+     								<th>Tanggal Pemesan</th>
+     								<th>Nama Pemesan</th>
+                    <th>Barang</th>
+                    <th>Quantity</th>
+     							</tr>
+     						</thead>
+     						<tbody>
+                  <?php while($permintaan = mysqli_fetch_assoc($result)) { ?>
+                    <tr>
+                      <td><?php echo $permintaan['id']; ?></td>
+                      <td><?php echo date('d F Y',strtotime($permintaan['tanggal'])); ?></td>
+    									<td><?php echo $permintaan['nama_pemesan']; ?></td>
+    									<td><?php echo $permintaan['produk']; ?></td>
+    									<td><?php echo $permintaan['jumlah']; ?></td>
+    								</tr>
+                    <?php } ?>
+     						</tbody>
+     					</table>
+     				</div>
+     			</div>
+         </div>
+       </div>
+     </div>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript">
+  			$(document).ready( function () {
+  			$('#table').DataTable({
+          dom: 'Bfrtip',
+          buttons: [
+            'print'
+          ],
+  				pageLength: 10,
+  				lengthMenu: [[5, 10, 20, -1], [5, 10, 15, 'All']],
+  				paging: true,
+  				ordering: true,
+  				stateSave: true,
+  				language: {
+  					"lengthMenu": "Show _MENU_" }
+  			});
+  		} );
+  	</script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js" charset="utf-8"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js" charset="utf-8"></script>
+    <script src="js/bootstrap.js" charset="utf-8"></script>
+  </body>
+</html>
+
+<!--
+Developed by Aditya Suryana
+github.com/adityasuryana
+2022
+-->
---
+++ laporan.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+if($_SESSION['status']!="loggedin"){
+  header("location:index.php");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laporan</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+       <div class="container">
+         <p class="navbar-brand mx-auto mb-0">kentang.</p>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+           <div class="navbar-nav ms-auto">
+             <a class="nav-link text-danger mx-2" href="logout.php">Logout</a>
+           </div>
+         </div>
+       </div>
+     </nav>
+     <?php
+       include 'connection.php';
+       $user = mysqli_query($conn,"select * from user");
+      ?>
+     <div class="container">
+       <div class="row">
+         <div class="col-12 mt-4">
+           <div class="d-flex">
+             <a href="owner.php" class="my-auto text-dark"><i class="fa-solid fa-arrow-left fs-20 me-3"></i></a>
+             <h2 class="title"><strong>Laporan</strong></h2>
+           </div>
+           <div class="dark-blue round-1 mt-4">
+             <div class="container p-2 px-3">
+               <div class="row">
+
+                 <div class="col-xxl-3 col-xl-3 col-lg-4 col-md-6 col-6 p-2">
+                   <a href="laporan-penjualan.php">
+                     <div class="card card-custom h-100">
+                       <div class="card-btn my-auto">
+                         <i class="icon fa-solid fa-file-invoice mb-2"></i>
+                         <p class="name-btn">Laporan Data Penjualan</p>
+                       </div>
+                     </div>
+                   </a>
+                 </div>
+
+                 <div class="col-xxl-3 col-xl-3 col-lg-4 col-md-6 col-6 p-2">
+                   <a href="laporan-permintaan.php">
+                     <div class="card card-custom h-100">
+                       <div class="card-btn my-auto">
+                         <i class="icon fa-solid fa-calculator mb-2"></i>
+                         <p class="name-btn">Laporan Data Permintaan</p>
+                       </div>
+                     </div>
+                   </a>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script src="js/bootstrap.js" charset="utf-8"></script>
+  </body>
+</html>
+
+<!--
+Developed by Aditya Suryana
+github.com/adityasuryana
+-->
